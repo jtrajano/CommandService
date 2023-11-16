42e3f437ad57bfe1c63ad4d297e801682acd0f88
Jeff Trajano <jlt212000@yahoo.com>
2023-11-16 10:36:44 +0800

    Initial Commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+    }
+}
